67f0895fc117a46c10c0a6f38f7c3144e537fdfe
Димитър Милков Харизанов <DMHarisanov23@codingburgas.bg>
2026-02-03 14:15:23 +0200

    Add cpp files

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,18 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include "library.h"
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Library lib;
+	lib.name = "My Library";
+	lib.count = 3;
+	lib.books = new Book[3];
+	lib.books[0] = createBook("1984", "George Orwell", 1949, 15.50);
+	lib.books[1] = createBook("The Hobbit", "J.R.R. Tolkien", 1937, 22.90);
+	lib.books[2] = createBook("Dune", "Frank Herbert", 1965, 18.00);
+	printLibrary(lib);
+	cout << "Average price: " << averageBookPrice(lib) << endl;
+	Book oldest = oldestBookInLibrary(lib);
+	cout << "Oldest book:" << endl;
+	printBook(oldest);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ StaticLib1/library.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "library.h"
+#include <iostream>
+using namespace std;
+Book createBook(const string& title,
+    const string& author,
+    int year,
+    double price)
+{
+    return { title, author, year, price };
+}
+void printBook(const Book& book)
+{
+    cout << "Title: " << book.title << endl;
+    cout << "Author: " << book.author << endl;
+    cout << "Year: " << book.year << endl;
+    cout << "Price: " << book.price << endl;
+}
+void printLibrary(const Library& library)
+{
+    for (int i = 0; i < library.count; i++) {
+        printBook(library.books[i]);
+    }
+}
+double averageBookPrice(const Library& library)
+{
+    if (library.count == 0) return 0;
+    double sum = 0;
+    for (int i = 0; i < library.count; i++) {
+        sum += library.books[i].price;
+    }
+    return sum / library.count;
+}
+Book oldestBookInLibrary(const Library& library)
+{
+    Book oldest = library.books[0];
+    for (int i = 1; i < library.count; i++) {
+        if (library.books[i].year < oldest.year)
+            oldest = library.books[i];
+    }
+    return oldest;
+}
