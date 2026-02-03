3ae85d2a12fb2afd0e52220c20b33fa431ffb8cf
Велизар Тодоров Вълков <vtvalkov23@codingburgas.bg>
2026-02-03 13:35:18 +0200

    add header file

---
+++ StaticLib1/library.h
@@ -0,0 +1,24 @@
+
+#pragma once
+#include <string>
+struct Book {
+    std::string title;
+    std::string author;
+    int year;
+    double price;
+};
+struct Library {
+    std::string name;
+    Book* books;
+    int count;
+};
+
+Book createBook(const std::string& title,
+    const std::string& author,
+    int year,
+    double price);
+void printBook(const Book& book);
+
+void printLibrary(const Library& library);
+double averageBookPrice(const Library& library);
+Book oldestBookInLibrary(const Library& library);
